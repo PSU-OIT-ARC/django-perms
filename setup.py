ebe3c2055a8e73696476e140443da6c209546298
Matt Johnson <mdj2@pdx.edu>
2014-03-07 12:50:52 -0800

    Version++

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="django-perms",
-    version='0.0.3',
+    version='0.0.5',
     url='https://github.com/PSU-OIT-ARC/django-perms',
     author='Matt Johnson',
     author_email='mdj2@pdx.edu',
