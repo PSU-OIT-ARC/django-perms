9f876084f9a81b0a8d92ed2a8416270369ab145a
Wyatt Baldwin <wyatt.baldwin@pdx.edu>
2015-03-25 10:45:08 -0700

    Update setup.py for imminent 1.0 release

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
-#!/usr/bin/env python
-from setuptools import setup
+import sys
+
+from setuptools import setup, find_packages
+
+
+django_version = '<1.7' if sys.version_info < (2, 7) else ''
+
+
 
 setup(
-    name="django-perms",
-    version='0.0.8',
+    name='django-perms',
+    version='1.0.0',
     url='https://github.com/PSU-OIT-ARC/django-perms',
     author='Matt Johnson',
     author_email='mdj2@pdx.edu',
-    description="Syntactic sugar for handling permission functions in views, templates and in code",
-    packages=['permissions', 'permissions.templatetags'],
+    maintainer='Wyatt Baldwin',
+    maintainer_email='wyatt.baldwin@pdx.edu',
+    description='Syntactic sugar for handling permission functions in views, templates and in code',
+    packages=find_packages(),
     zip_safe=False,
     extras_require={
         'dev': [
             'coverage',
-            'django',
+            'django{version}'.format(version=django_version),
             'django-nose',
             'nose',
             'six',
@@ -22,6 +30,10 @@ setup(
     classifiers=[
         'Framework :: Django',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
